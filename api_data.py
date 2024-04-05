c757f2b239fb37ca2827dfa35d9b03ee6ddcff39
TinChung41 <tinchung21@gmail.com>
2024-04-05 12:40:43 +0700

    prepare for pipe line with Terraform

--- api_data.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo'
-r = requests.get(url)
-data = r.json()
-
-print(data)
